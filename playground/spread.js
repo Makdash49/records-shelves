7f29e2229828a6ecf674a767871410dd974f8336
Mark McQuillen <markdmcq@hotmail.com>
2016-10-20 19:30:15 -0700

    Add playground for spread operator

---
+++ playground/spread.js
@@ -0,0 +1,44 @@
+// function add (a, b) {
+//   return a + b
+// }
+//
+// console.log(add(3, 1));
+//
+//
+// var toAdd = [9, 5];
+//
+// console.log(add(...toAdd));
+
+
+// var groupA = ["Bob", "Anne"];
+// var groupB = ["Vikram"];
+// var final = [...groupB, 3, ...groupA];
+//
+// console.log(final)
+
+
+var person = ['Andrew', 25];
+var personTwo = ['Jen', 29];
+// Hi Andrew, you are 25.
+
+var nameAge = function (name, age) {
+  console.log("Hi " + name + ", you are " + age)
+};
+
+nameAge(...person);
+nameAge(...personTwo);
+
+
+
+var names = ['Mike', 'Ben'];
+var final = [...names, 'Mark'];
+// Hi Mark - use for each.
+
+
+for (var i = 0; i < final.length; i++) {
+  console.log("Hi " + final[i])
+};
+
+final.forEach(function (name) {
+  console.log("Yoooo " + name)
+});
