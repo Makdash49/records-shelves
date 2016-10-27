6b050dc51346130f8b56d038a53cb8ee095c3963
Mark McQuillen <markdmcq@hotmail.com>
2016-10-27 13:34:23 -0700

    Update firebase with set

---
+++ playground/firebase/index.js
@@ -0,0 +1,37 @@
+import firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyCQ8TrtZN1cNKyu12JSkLG59dWRMmoReek",
+  authDomain: "mcquillen-react-project.firebaseapp.com",
+  databaseURL: "https://mcquillen-react-project.firebaseio.com",
+  storageBucket: "mcquillen-react-project.appspot.com",
+  messagingSenderId: "122006660498"
+};
+firebase.initializeApp(config);
+
+var firebaseRef = firebase.database().ref();
+
+firebaseRef.set({
+  app: {
+    name: 'Todo App',
+    version: '1.0'
+  },
+  isRunning: true,
+  user: {
+    name: 'Mark',
+    age: 43
+  }
+}).then(()=>{
+  console.log('Set worked!');
+}, (e) => {
+  console.log('Set failed');
+});
+
+firebaseRef.child('user').set({
+  name: 'Abe'
+});
+
+
+firebaseRef.child('app').set({
+  name: 'Super Todo App'
+});
