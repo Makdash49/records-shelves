6581b8f7b5094810a4e754ac0bebd7d899ec2510
Mark McQuillen <markdmcq@hotmail.com>
2016-10-30 21:48:33 -0700

    my solution to test startAddTodos challenge

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -84,22 +84,54 @@ describe('Actions', () => {
     expect(res).toEqual(action);
   });
 
-
-  describe('Tests with fireball todos', () => {
+  describe('Tests with firebase todos', () => {
     var testTodoRef;
 
     beforeEach((done) => {
-      testTodoRef = firebaseRef.child('todos').push();
+      var todosRef = firebaseRef.child('todos');
 
-      testTodoRef.set({
-        text: 'Something to do',
-        completed: false,
-        createdAt: 23452353
-      }).then(() => done());
+      todosRef.remove().then(() => {
+        testTodoRef = firebaseRef.child('todos').push();
+
+        return testTodoRef.set({
+          text: 'Something to do',
+          completed: false,
+          createdAt: 23452353
+        })
+      })
+      .then(() => done())
+      .catch(done);
     });
 
     afterEach((done) => {
       testTodoRef.remove().then(() => done());
+    });
+
+
+
+
+
+
+    it('should do a successful startAddTodo action', (done) => {
+
+      const store = createMockStore({});
+      const action = actions.startAddTodos();
+
+      store.dispatch(action).then(() => {
+        const mockActions = store.getActions();
+
+        expect(mockActions[0]).toInclude({
+          type: 'ADD_TODOS'
+        });
+
+        expect(mockActions[0]['todos']).toExist();
+
+        expect(mockActions[0]['todos'][0]['text']).toEqual('Something to do');
+
+        expect(mockActions[0]['todos'].length).toEqual(1);
+
+        done();
+      }, done);
     });
 
     it('should toggle todo and dispatch UPDATE_TODO action', (done) => {
