099d624cc41af01602a51ea541090d41e1d51e60
Mark McQuillen <markdmcq@hotmail.com>
2016-10-31 13:08:41 -0700

    Move firebase credentials to environment variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,12 +2,13 @@ import firebase from 'firebase';
 
 try {
   var config = {
-    apiKey: "AIzaSyCQ8TrtZN1cNKyu12JSkLG59dWRMmoReek",
-    authDomain: "mcquillen-react-project.firebaseapp.com",
-    databaseURL: "https://mcquillen-react-project.firebaseio.com",
-    storageBucket: "mcquillen-react-project.appspot.com",
-    messagingSenderId: "122006660498"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
   };
+
   firebase.initializeApp(config);
 } catch (e) {
 
