de359ae8a92a89b64f0e39edec5eb83c17931c2e
Mark McQuillen <markdmcq@hotmail.com>
2016-10-30 22:27:46 -0700

    put back code in todoList and now it is loading from firebase

--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -7,23 +7,11 @@ export var TodoList = React.createClass({
   render: function () {
     var {todos, showCompleted, searchText} = this.props;
     var renderTodos = () => {
-
-      var allCompleted = true;
-
-      todos.forEach(function(todo){
-        if (!todo.completed) {
-          allCompleted = false
-        };
-      });
-
-      console.log('allCompleted????????', allCompleted)
-
-      if (allCompleted) {
+      if (todos.length === 0) {
         return (
           <p className="container__message">Nothing To Do</p>
         );
       }
-
       return TodoAPI.filterTodos(todos, showCompleted, searchText).map((todo) => {
         return (
           <Todo key={todo.id} {...todo}/>
