dc57372e8f360dc21f0bb9a6a357e169361303f0
Mark McQuillen <markdmcq@hotmail.com>
2016-10-30 23:37:47 -0700

    Andrews solution using filter, my solution works better

--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -6,29 +6,16 @@ var TodoAPI = require('TodoAPI');
 export var TodoList = React.createClass({
   render: function () {
     var {todos, showCompleted, searchText} = this.props;
-    var allCompleted = true;
-
-    todos.forEach(function(todo){
-      if (!todo.completed) {
-        allCompleted = false
-      };
-    });
-
-    var renderMessage = () => {
-      if (allCompleted && showCompleted) {
-        return (
-          <p className="container__message">Nothing To Do</p>
-        );
-      };
-    };
-
     var renderTodos = () => {
-      if (allCompleted && !showCompleted) {
+      var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
+
+      if (filteredTodos.length === 0) {
         return (
           <p className="container__message">Nothing To Do</p>
         );
       }
-      return TodoAPI.filterTodos(todos, showCompleted, searchText).map((todo) => {
+
+      return filteredTodos.map((todo) => {
         return (
           <Todo key={todo.id} {...todo}/>
         );
@@ -37,7 +24,6 @@ export var TodoList = React.createClass({
 
     return (
       <div>
-        {renderMessage()}
         {renderTodos()}
       </div>
     )
