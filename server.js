342f322edba2d03139c85566fa1f6277714acfc7
Mark McQuillen <markdmcq@hotmail.com>
2016-12-07 21:36:14 -0800

    Use environment variables thanks to node-env-file

--- server.js
+++ server.js
@@ -1,5 +1,31 @@
 var express = require('express');
 
+var envFile = require('node-env-file');
+envFile('./config/development.env');
+
+var amazon = require('amazon-product-api');
+var amazonData;
+var client = amazon.createClient({
+  awsId: process.env.AWS_ID,
+  awsSecret: process.env.AWS_SECRET,
+  awsTag: "Todo App"
+});
+
+client.itemSearch({
+  // director: 'Quentin Tarantino',
+  // actor: 'Samuel L. Jackson',
+  // searchIndex: 'DVD',
+  // audienceRating: 'R',
+  // responseGroup: 'ItemAttributes,Offers,Images'
+  keywords: 'Robocop miniature'
+}).then(function(results){
+  console.log(JSON.stringify(results[0]["ItemAttributes"][0]["Title"][0]));
+  amazonData = results
+}).catch(function(err){
+  console.log(err);
+});
+
+
 //Create our app
 
 var app = express();
