c3787101062ff361e2f903ba3a19227881b12bb1
Mark McQuillen <markdmcq@hotmail.com>
2016-12-19 16:54:06 -0800

    Fix column sorting by mulitplying quotient by 100 for the sake of round

--- app/api/ProductAPI.jsx
+++ app/api/ProductAPI.jsx
@@ -48,7 +48,7 @@ module.exports = {
     return newFilteredProducts;
   },
   totalFilterProducts: function (products, userOneTotal, userTwoTotal) {
-    console.log('PRODUCTS - PRODUCTS - PRODUCST', products);
+    // console.log('PRODUCTS - PRODUCTS - PRODUCST', products);
     var filteredProducts = products;
 
     var newFilteredProducts = filteredProducts.sort((a, b) => {
@@ -60,22 +60,23 @@ module.exports = {
     });
 
     newFilteredProducts.sort((a, b) => {
-      var apercentageUserOne = Math.round((a.counterUserOne / userOneTotal));
-      var apercentageUserTwo = Math.round((a.counterUserTwo / userTwoTotal));
+      var apercentageUserOne = Math.round((a.counterUserOne / userOneTotal) * 100);
+      var apercentageUserTwo = Math.round((a.counterUserTwo / userTwoTotal) * 100);
 
 
       isNaN(apercentageUserOne) ? apercentageUserOne = 0 : apercentageUserOne = apercentageUserOne;
       isNaN(apercentageUserTwo) ? apercentageUserTwo = 0 : apercentageUserTwo = apercentageUserTwo;
       var aFinalPercentage = apercentageUserOne + apercentageUserTwo;
+      console.log('aFinalPercentage', aFinalPercentage);
 
 
-      var bpercentageUserOne = Math.round((b.counterUserOne / userOneTotal));
-      var bpercentageUserTwo = Math.round((b.counterUserTwo / userTwoTotal));
+      var bpercentageUserOne = Math.round((b.counterUserOne / userOneTotal) * 100);
+      var bpercentageUserTwo = Math.round((b.counterUserTwo / userTwoTotal) * 100);
 
       isNaN(bpercentageUserOne) ? bpercentageUserOne = 0 : bpercentageUserOne = bpercentageUserOne;
       isNaN(bpercentageUserTwo) ? bpercentageUserTwo = 0 : bpercentageUserTwo = bpercentageUserTwo;
       var bFinalPercentage = bpercentageUserOne + bpercentageUserTwo;
-
+      console.log('bFinalPercentage', bFinalPercentage);
 
       if(aFinalPercentage > bFinalPercentage) {
         return -1;
