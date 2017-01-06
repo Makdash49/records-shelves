a646cb29d4b5209de0d8d3a67f2f751c6a5c2a2e
Mark McQuillen <markdmcq@hotmail.com>
2017-01-06 12:23:58 -0800

    Add about bubble

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -17,9 +17,14 @@ export class TodoApp extends React.Component {
   render() {
     return (
       <div>
-        <div className="about-description">
-          <p>This is a cool app that will do some really really neat things.</p>
-          <p>It allows users to vote for what they want in a way that lets them express how much they like their choices in comparison to each other.</p>
+        <div className="about">
+          <h4 className="page-title">GiftVote - The Collaborative Shopping Platform</h4>
+          <ul>
+            <li>Enables groups of users to discover products they most prefer through live voting.</li>
+            <li>Incorporates Amazon-Product-API into Node.js to display product information.</li>
+            <li>Enables real-time updates to all users simultaneously utilizing Firebase database and event listeners.</li>
+            <li>Calculates and displays user's individual vote percentage and sum of all users’ percentages with JavaScript algorithm.</li>
+          </ul>
         </div>
       </div>
     )
