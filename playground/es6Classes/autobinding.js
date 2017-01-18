4d19cbab376b287aee7cc9add2bc621b69e82b7f
Mark McQuillen <markdmcq@hotmail.com>
2017-01-18 09:06:35 -0800

    Delete playground

--- playground/es6Classes/autobinding.js
+++
@@ -1,9 +0,0 @@
-var obj = {
-  name: 'Andrew',
-  printName: function () {
-    // // // // // // console.log(`My name is ${this.name}`);
-  }
-};
-
-obj.printName();
-setTimeout(obj.printName.bind({name: 'Mike'}), 1000);
