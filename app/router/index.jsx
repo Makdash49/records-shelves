deafa1b6fd6f137a5df6d415097799e541d2bbf6
Mark McQuillen <markdmcq@hotmail.com>
2017-01-18 09:40:44 -0800

    Switch to Main

--- app/router/index.jsx
+++ app/router/index.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import {Route, Router, IndexRoute, hashHistory} from 'react-router';
 
-import Login from 'Login';
-
-// var Main = require('Main');
-
+import Main from 'Main';
 
 export default (
   <Router history={hashHistory}>
-    <Route path="/" component={Login}>
+    <Route path="/" component={Main}>
     </Route>
   </Router>
 );
